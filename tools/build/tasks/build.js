e0a93ca10561ed400fa3908e3e25a6688833ffca
wlchair <wulei0114@126.com>
2016-06-08 15:29:59 +0800

    update manual reg replace file to automatic replace file

--- tools/build/tasks/build.js
+++ tools/build/tasks/build.js
@@ -8,28 +8,16 @@ var requirejs = require('requirejs'),
     fs = require('fs'),
     path = require('path');
 // convert relative path to absolute path.
-function convert( name, _path, contents ) {
-    // var rDefine = /(define\s*\(\s*('|").*?\2\s*,\s*\[)([\s\S]*?)\]/ig,
-    //     rDeps = /('|")(.*?)\1/g,
-    //     root = _path.substr( 0, _path.length - name.length - 3 ),
-    //     dir = path.dirname( _path ),
-    //     m, m2, deps, dep, _path2;
-
-    // contents = contents.replace( rDefine, function( m, m1, m2, m3 ) {
-    //     return m1 + m3.replace( rDeps, function( m, m1, m2 ) {
-    //         m2 = path.join( dir, m2 );
-    //         m2 = path.relative( root, m2 );
-
-    //         m2 = m2.replace(/\\/g, '/');
-    //         return m1 + m2 + m1;
-    //     }) + ']';
-    // });
-
+function convert(name, _path, contents) {
+    var rDep = /\,\ ?\[.*\]\,function\(\ ?.*\ ?\)/ig,
+        methodNotesReg = /\/\*(\s|.)*?\*\//ig;
+    contents = contents.replace(rDep, ',function()');
+    contents = contents.replace(methodNotesReg, '');
     return contents;
 }
 
-module.exports = function( grunt ) {
-    grunt.registerMultiTask( 'build', '合并amd modules为一个单文件', function() {
+module.exports = function(grunt) {
+    grunt.registerMultiTask('build', '合并amd modules为一个单文件', function() {
 
         var done = this.async(),
             options = this.options({
@@ -67,15 +55,15 @@ module.exports = function( grunt ) {
 
             rawText: {},
 
-            onBuildWrite: function( name, _path ) {
-                var compiled = convert.apply( null, arguments );
+            onBuildWrite: function(name, _path) {
+                var compiled = convert.apply(null, arguments);
 
-                if ( options.process ) {
-                    compiled = options.process( compiled, _path );
+                if (options.process) {
+                    compiled = options.process(compiled, _path);
                 }
 
                 // 调整缩进
-                compiled = compiled.replace( /(^|\r\n|\r|\n)/g, '$1    ');
+                compiled = compiled.replace(/(^|\r\n|\r|\n)/g, '$1    ');
 
                 compiled = compiled.replace(/@version@/g, function() {
                     return pkg.version;
@@ -88,34 +76,34 @@ module.exports = function( grunt ) {
             include: []
         };
 
-        options = grunt.util._.extend( options, this.data );
-        config.name = 'webuploader';
-        if ( options.builtin.dollar ) {
+        options = grunt.util._.extend(options, this.data);
+        config.name = 'spreadsheet';
+        if (options.builtin.dollar) {
             config.rawText.dollar = 'define([\n' +
-                    '    \'./dollar-builtin\'\n' +
-                    '], function( $ ) {\n' +
-                    '    return $;\n' +
-                    '});';
+                '    \'./dollar-builtin\'\n' +
+                '], function( $ ) {\n' +
+                '    return $;\n' +
+                '});';
         }
 
-        if ( options.builtin.promise ) {
+        if (options.builtin.promise) {
             config.rawText.promise = 'define([\n' +
-                    '    \'./promise-builtin\'\n' +
-                    '], function( $ ) {\n' +
-                    '    return $;\n' +
-                    '});';
+                '    \'./promise-builtin\'\n' +
+                '], function( $ ) {\n' +
+                '    return $;\n' +
+                '});';
         }
 
-        if ( this.data.preset === 'custom' ) {
+        if (this.data.preset === 'custom') {
             custom = [];
 
-            this.files.forEach(function( file ) {
+            this.files.forEach(function(file) {
                 var files = file.src,
                     cwd = file.cwd || '';
 
-                files.filter(function( filepath ) {
+                files.filter(function(filepath) {
 
-                    filepath = path.join( cwd, filepath );
+                    filepath = path.join(cwd, filepath);
                     // Warn on and remove invalid source files (if nonull was set).
                     if (!grunt.file.exists(filepath)) {
                         grunt.log.warn('Source file "' + filepath + '" not found.');
@@ -123,8 +111,8 @@ module.exports = function( grunt ) {
                     } else {
                         return true;
                     }
-                }).forEach(function( filepath ) {
-                    custom.push( '\'' + filepath.replace( /\.\w+$/, '' ) + '\'' );
+                }).forEach(function(filepath) {
+                    custom.push('\'' + filepath.replace(/\.\w+$/, '') + '\'');
                 });
             });
 
@@ -134,36 +122,37 @@ module.exports = function( grunt ) {
         } else if (this.data.preset) {
 
             config.rawText.webuploader = 'define([\n    ' + ['\'./preset/' +
-                    this.data.preset +'\''].join(',\n    ') +
-                    '\n], function( preset ) {\n    return preset;\n});';
+                    this.data.preset + '\''
+                ].join(',\n    ') +
+                '\n], function( preset ) {\n    return preset;\n});';
         } else {
             config.name = this.data.name;
         }
 
         // 处理最终输出
-        config.out = function( compiled ) {
+        config.out = function(compiled) {
             var arr = [],
                 banner = grunt.template.process(options.banner),
                 footer = grunt.template.process(options.footer),
                 sep = '\n\n';
 
-            banner && arr.push( banner );
+            banner && arr.push(banner);
 
-            if ( options.builtin.dollar ) {
+            if (options.builtin.dollar) {
                 compiled = compiled.replace('define([ \'jquery\' ], exports );', 'define([], exports);');
             }
 
             arr.push(compiled);
-            footer && arr.push( footer );
+            footer && arr.push(footer);
 
             // Write concatenated source to file
-            grunt.file.write( dest, arr.join( sep ) );
+            grunt.file.write(dest, arr.join(sep));
 
             process.nextTick(function() {
                 // requirejs有bug, callback不一定会执行，目前调试的结果是
                 // prim的promise实现有问题。
-                if ( flag ) return;
-                grunt.log.ok( "File '" + dest + "' created." );
+                if (flag) return;
+                grunt.log.ok("File '" + dest + "' created.");
                 done();
                 flag = true;
             });
@@ -171,21 +160,21 @@ module.exports = function( grunt ) {
 
         if (options.fis) {
             config.wrap = {
-                startFile: 'build/fis/intro.js',
-                endFile: 'build/fis/outro.js'
+                startFile: 'tools/build/fis/intro.js',
+                endFile: 'tools/build/fis/outro.js'
             }
         }
 
-        requirejs.optimize( config, function( response ) {
+        requirejs.optimize(config, function(response) {
             // requirejs有bug, callback不一定会执行，目前调试的结果是
             // prim的promise实现有问题。
-            if ( flag ) return;
-            grunt.verbose.writeln( response );
-            grunt.log.ok( "File '" + name + "' created." );
+            if (flag) return;
+            grunt.verbose.writeln(response);
+            grunt.log.ok("File '" + name + "' created.");
             done();
             flag = true;
-        }, function( err ) {
-            done( err );
+        }, function(err) {
+            done(err);
         });
     });
 };
