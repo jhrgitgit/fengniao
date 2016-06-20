755f91940de03d94ba7d3e79dbb21f7c32881db0
wlchair <wulei0114@126.com>
2016-06-20 17:12:04 +0800

    del mongdb files

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var application_root = __dirname,
 	express = require('express'),
 	bodyParser = require('body-parser'),
-	path = require('path'),
-	mongoose = require('mongoose');
+	path = require('path');
+	// mongoose = require('mongoose');
 
 var app = express();
 // mongoose.connect('mongodb://localhost/library_database');
