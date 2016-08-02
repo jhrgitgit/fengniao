1e663874b70bf721f40ade9853a545e18437288f
wlchair <wulei0114@126.com>
2016-08-02 14:17:54 +0800

    feat(doc): addition build changelog function
    import `grunt-bump` plugins and convention-changelog function, for auto build `CHANGELOG.md` file
    and auto build new version for tag

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,16 +41,54 @@ module.exports = function(grunt) {
                     promise: false
                 }
             }
-        }
+        },
+	bump: {
+	    options: {
+		updateConfigs: ['pkg'],
+		commitFIles:['package.json', 'CHANGELOG.md'],
+		commitMessage: 'spreadsheet: release v%VERSION%'
+	    }
+	},
+	conventionalChangelog: {
+	    options: {
+	      changelogOpts: {
+		// conventional-changelog options go here
+		preset: 'angular',
+		outputUnreleased:true
+	      },
+	      context: {
+		// context goes here
+	      },
+	      gitRawCommitsOpts: {
+		// git-raw-commits options go here
+	      },
+	      parserOpts: {
+		// conventional-commits-parser options go here
+	      },
+	      writerOpts: {
+		// conventional-changelog-writer options go here
+	      }
+	    },
+	    release: {
+	      src: 'CHANGELOG.md'
+	    }
+	}
     });
     require('load-grunt-tasks')(grunt);
     grunt.loadTasks('tools/build/tasks'); // 加载build目录下的所有task
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-conventional-changelog');
 
     grunt.registerTask('check', ['jshint']);
     grunt.registerTask('dist', ['build']);
     grunt.registerTask('default', ['jshint', 'build']);
     grunt.registerTask('css', ['less']);
+    grunt.registerTask('release','build new version info',function(type){
+	grunt.task.run([
+	    'bump:' + (type || 'patch') + ':bump-only',
+	    'conventionalChangelog',
+	    'bump-commit'
+	]);
+    });
 };
