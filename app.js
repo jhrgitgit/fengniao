70d684519c87adc8adb2e3b11e64f17b03b4a6c8
CJL5678 <2737812732@qq.com>
2016-09-07 14:35:34 +0800

    修改由于jsp与html路径不同，造成的项目错误

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 'use strict';
 requirejs.config({
-	baseUrl: '../js'
+	baseUrl: './js'
 });
 define(function(require) {
 	var $ = require('lib/jquery'),
 		SpreadSheet = require('spreadsheet/spreadsheet');
-		
+
 	window.SPREADSHEET_AUTHENTIC_KEY = $('#excelId').val();
 	window.SPREADSHEET_BUILD_STATE = $('#build').val();
-	var ss =new SpreadSheet('spreadsheet');
-	$('#t').on('click',function(){
-		ss.colHide();
-	});
-	$('#tt').on('click',function(){
-		ss.colCancelHide();
-	});
+	var ss = new SpreadSheet('spreadSheet');
 });
